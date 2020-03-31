f1de5416b1e039697bde9007fec54958d64d1651
kaur7428 <59924432+kaur7428@users.noreply.github.com>
2020-03-30 23:20:25 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -12,15 +12,47 @@ package ca.sheridancollege.project;
  * @author dancye, 2018
  */
 public abstract class Card 
-{
-    //default modifier for child classes
-    
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    
-    @Override
-    public abstract String toString();
-    
+{ private final Color color;
+	private final Value value;
+	private int attribute;
+
+	public Color getColor() {
+		return this.color;
+	}
+   public Card(Color s, Value val) {
+		// TODO - implement Card.Card
+		throw new UnsupportedOperationException();
+	}
+
+	public Value getValues() {
+		// TODO - implement Card.getValues
+		throw new UnsupportedOperationException();
+	}
+
+
+	/**
+	 * Students should implement this method for their specific children classes
+	 * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+	 */
+	public enum Color {
+		RED,
+		BLUE,
+		GREEN,
+		YELLOW
+	}
+
+
+	public enum Value {
+		ZERO,
+		ONE,
+		TWO,
+		THREE,
+		FOUR,
+		FIVE,
+		SIX,
+		SEVEN,
+		EIGHT,
+		NINE
+	}
+
 }
